8d512cdc6d7f0245b329c9621993587217d94d15
Malik Sari <Malik.Sari@outlook.com>
2021-07-28 12:49:39 +0200

    Initial commit

---
+++ src/schleifen1.java
@@ -0,0 +1,23 @@
+public class schleifen1 {
+
+    public static void main(String[] args) {
+        int age = 76;
+
+        if (age >= 18) {
+
+            System.out.println("u are an adult");
+        }
+
+        else if (age>=18){
+            System.out.println("ok boomer");
+        }
+
+        else if (age>=13){
+            System.out.println("you are a Teenager");
+        }
+        else{
+
+            System.out.println("u are a child");
+        }
+    }
+}
